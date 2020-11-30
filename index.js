ee571f493f85c66588ba08e17b7b2ae402b19348
hugodelgado <hugo.delgado1@outlook.com>
2020-11-30 09:50:33 -0600

    commit tree a17f63afb2791449e583ee632d40e15916c1f114

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function fetchBooks() {
 
+  fetch(`https://anapioficeandfire.com/api/books`)
+  .then((response) => response.json())
+  .then((books) => {
+    books.forEach(book => 
+      renderBooks(book))
+  })
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
+  const h2 = document.createElement('h2')
+    h2.innerHTML = books.name
     main.appendChild(h2)
-  })
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
